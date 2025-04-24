d3311d2bee3613e983bd50c44421171a2e744bed
LucCharb <45023900+LucCharb@users.noreply.github.com>
2025-04-24 02:21:48 +0000

    pam-setup.sh

--- installs/decrypt/pam-setup.sh
+++ installs/decrypt/pam-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# NEED TO INSTALL pam
+echo "**************  NEED TO INSTALL pam *************"
 echo "Insert line 'auth optional pam_exec.so expose_authtok /usr/local/bin/decrypt.sh' on top of /etc/pam.d/system-login"
 # auth optional pam_exec.so expose_authtok /usr/local/bin/decrypt.sh
-sudo cp /mnt/2TB-2/Linux/Arch/installs/decrypt/decrypt.sh /usr/local/bin/
+sudo cp $HOME/luclinux/installs/decrypt/decrypt.sh /usr/local/bin/
 
 #echo "This is the new first line" | cat - /mnt/2TB-2/Linux/Arch/installs/decrypt.sh > /mnt/2TB-2/Linux/Arch/installs/decrypt-tmp.sh && mv /mnt/2TB-2/Linux/Arch/installs/decrypt-tmp.sh /mnt/2TB-2/Linux/Arch/installs/decrypt.sh
 chmod +x /usr/local/bin/decrypt.sh
